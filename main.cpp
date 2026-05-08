b3c062ad7631b051f8b0c27acf323f1c6e49e954
Christopher Syduel Maita Tovar <csmaita.25@est.ucab.edu.ve>
2026-05-08 13:58:55 -0400

    v0.0.0.2

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+/* PROYECTO DE JUEGO - ALGORITMOS Y ESTRUCTURAS DE DATOS
+    REALIZADO POR CHRISTOPHER MAITA - 31_761_223*/
+
 #include <iostream>
 #include "lib.h"
 
@@ -5,7 +8,6 @@ using namespace std;
 
 int main(){
     cout << "hola mundo";
-
 
 
 
